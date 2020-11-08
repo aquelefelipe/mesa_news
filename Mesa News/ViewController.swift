441ab907b8aa05a1355762639b71f9564b72f460
Felipe Figueiroa <figueiroa.fe@gmail.com>
2020-11-08 12:42:30 -0300

    start project - first commit

---
+++ Mesa News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mesa News
+//
+//  Created by Felipe Figueirôa on 08/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
